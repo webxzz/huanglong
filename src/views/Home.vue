83a5b9fe2b8d4ca64853f9481cbaf5460fabd559
first <2145920341@qq.com>
2021-11-05 16:48:16 +0800

    第一次提交

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="wrap">
           <header class="header">
-
+              <div class="header_top">
+                <input type="text" v-model="inp" placeholder="欢迎来到紧缺应用技术技能型人才职业教育报考平台">
+              </div>
           </header>
           <main class="main">
 
@@ -14,11 +16,20 @@
 
 <script>
   export default {
-
+      data(){
+        return{
+          inp:''
+        }
+      }
   }
 </script>
 
 <style lang="scss" scoped>
+input{
+  background:none;  
+	outline:none;  
+	border:none;
+}
 .wrap{
   height: 100%;
   display: flex;
@@ -27,6 +38,24 @@
       width: 3.75rem;
       // height: 0.4rem;
       background: red;
+      .header_top{
+        width: 3.75rem;
+        height: 0.3rem;
+        background: paleturquoise;
+        display: flex;
+        input{
+          width: 275px;
+          height: 0.3rem;
+          font-family: PingFang-SC-Medium;
+          font-size: 12px;
+          font-weight: normal;
+          font-stretch: normal;
+          letter-spacing: 0px;
+          color: #777777;
+          margin: 0 0 0 0.13rem;
+
+        }
+      }
   }
   .main{
      flex: 1;
